907c6b2b1571a09229c5aa6d3097c9ad914e3a85
Hithru <56365929+Hithru@users.noreply.github.com>
2021-09-21 11:38:29 +0530

    PWA google chrome add

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,31 +1,15 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./App.css";
+import video from "./videos/sculpture.mp4";
 
 function App() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await fetch("https://orangevalleycaa.org/api/videos").then(
-        (response) => response.json()
-      );
-
-      setData(result);
-    };
-
-    fetchData();
-  }, []);
-
   return (
     <div className="App">
       <header>
         <h1>Art Videos</h1>
-        {data.map((video) => (
-          <div key={video.id}>
-            <h2>{video.name}</h2>
-            <video height={200} controls src={video.video_url} />
-          </div>
-        ))}
+        <div>
+          <video src={video} controls height={200} />
+        </div>
       </header>
     </div>
   );
